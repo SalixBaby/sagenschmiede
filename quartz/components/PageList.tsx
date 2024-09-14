9d1381120892fdb61c8ab1214c6ec0939fc4c204
salixbaby <hendrik.schmuelling@googlemail.com>
2024-09-14 22:00:50 +0200

    Quartz sync: Sep 14, 2024, 10:00 PM

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -46,13 +46,6 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
         return (
           <li class="section-li">
             <div class="section">
-              <div>
-                {page.dates && (
-                  <p class="meta">
-                    <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                  </p>
-                )}
-              </div>
               <div class="desc">
                 <h3>
                   <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
