86d6ac542422e264e904564148660ad32748b03a
salixbaby <hendrik.schmuelling@googlemail.com>
2024-09-14 21:51:00 +0200

    Quartz sync: Sep 14, 2024, 9:51 PM

--- quartz/i18n/locales/de-DE.ts
+++ quartz/i18n/locales/de-DE.ts
@@ -65,7 +65,7 @@ export default {
     error: {
       title: "Nicht gefunden",
       notFound: "Diese Seite ist entweder nicht öffentlich oder existiert nicht.",
-      home: "Return to Homepage",
+      home: "Zurück zur Hauptseite",
     },
     folderContent: {
       folder: "",
